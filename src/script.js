7d6b55cd64a556f29a84b552b48dc2c067c0c7e0
Daniela Heiß <heiss_dani@yahoo.com>
2024-03-14 14:49:06 +0100

    Added Jurij's changes manually

--- src/script.js
+++ src/script.js
@@ -1,29 +1,39 @@
 function createDivs() {
-            const container = document.getElementById("features-main-container");
-            let numberOfIterations = 6;
+        const container = document.getElementById("features-main-container");
+        let numberOfIterations = 6;
 
-            for (let i = 1; i < numberOfIterations;){
-                    const div1 = document.createElement("div");
-                    div1.className = "line";
-                    div1.style.gridArea = "graph"+i;  
-                    i++;          
-            
-                    const div2 = document.createElement("div");
-                    div2.className = "line2";
-                    div2.style.gridArea = "branch"+i;
+                const divline = document.createElement("div");
+                divline.id = "line";
+                divline.style.gridArea = "graph1";
 
-                    const div3 = document.createElement("div");
-                    div3.className = "dot2";
-                    div3.style.gridArea = "branch"+i;
+                container.appendChild(divline);
+                         
 
-                    const div4 = document.createElement("div");
-                    div4.className = "dot";
-                    div4.style.gridArea = "graph"+i;
+        for (let i = 1; i < numberOfIterations;){
+                if(i>1){
+                const div1 = document.createElement("div");
+                div1.className = "line";
+                div1.style.gridArea = "graph"+i;
+                container.appendChild(div1);
+                };
+                i++;          
+        
+                const div2 = document.createElement("div");
+                div2.className = "line2";
+                div2.style.gridArea = "branch"+i;
 
-                    container.appendChild(div1);
-                    container.appendChild(div2);
-                    container.appendChild(div3);
-                    container.appendChild(div4);
-            }
-            
+                const div3 = document.createElement("div");
+                div3.className = "dot2";
+                div3.style.gridArea = "branch"+i;
+
+                const div4 = document.createElement("div");
+                div4.className = "dot";
+                div4.style.gridArea = "graph"+i;
+
+                                  
+                container.appendChild(div2);
+                container.appendChild(div3);
+                container.appendChild(div4);
+        }
+        
 }
