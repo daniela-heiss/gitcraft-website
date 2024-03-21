ce41bfc595737c78b38436c55b73f1d1a2e80fa0
Daniela Heiß <heiss_dani@yahoo.com>
2024-03-21 13:08:49 +0100

    Improved contrast with using white text instead of dark text in header and footer

--- src/script.js
+++ src/script.js
@@ -3,43 +3,43 @@ function createDivs() {
         let numberOfIterations = 6;
 
         const divstart = document.createElement("div");
-                divstart.id = "graphstart";
-                divstart.style.gridArea = "graph1";
+        divstart.id = "graphstart";
+        divstart.style.gridArea = "graph1";
 
-                container.appendChild(divstart);
+        container.appendChild(divstart);
 
-                const divline = document.createElement("div");
-                divline.id = "line";
-                divline.style.gridArea = "graph1";
+        const divline = document.createElement("div");
+        divline.id = "line";
+        divline.style.gridArea = "graph1";
 
-                container.appendChild(divline);
-                         
+        container.appendChild(divline);
 
-        for (let i = 1; i < numberOfIterations;){
-                if(i>1){
-                const div1 = document.createElement("div");
-                div1.className = "line";
-                div1.style.gridArea = "graph"+i;
-                container.appendChild(div1);
+
+        for (let i = 1; i < numberOfIterations;) {
+                if (i > 1) {
+                        const div1 = document.createElement("div");
+                        div1.className = "line";
+                        div1.style.gridArea = "graph" + i;
+                        container.appendChild(div1);
                 };
-                i++;          
-        
+                i++;
+
                 const div2 = document.createElement("div");
                 div2.className = "line2";
-                div2.style.gridArea = "branch"+i;
+                div2.style.gridArea = "branch" + i;
 
                 const div3 = document.createElement("div");
                 div3.className = "dot2";
-                div3.style.gridArea = "branch"+i;
+                div3.style.gridArea = "branch" + i;
 
                 const div4 = document.createElement("div");
                 div4.className = "dot";
-                div4.style.gridArea = "graph"+i;
+                div4.style.gridArea = "graph" + i;
 
-                                  
+
                 container.appendChild(div2);
                 container.appendChild(div3);
                 container.appendChild(div4);
         }
-        
+
 }
